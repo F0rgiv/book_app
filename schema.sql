f42d44579e61bd5b6233737b2e0152d415876323
James Mansour <jamestmansour@outlook.com>
2021-03-02 16:28:37 -0800

    Able to save books

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ DROP TABLE book;
 
 CREATE TABLE book (
     id SERIAL PRIMARY KEY,
-    isbn VARCHAR(13),
+    isbn VARCHAR(255),
     img_url VARCHAR(255),
     title VARCHAR(255),
     author VARCHAR(255),
--- server.js
+++ server.js
@@ -23,12 +23,14 @@ app.get('/hello', home);
 app.get('/searches/new', getBookQurie)
 app.post('/searches/new', getBooks)
 app.get('/books/:id', getBook)
-// app.post('/book', saveBook)
+app.post('/books', saveBook)
 
 // ======================================= Rout Handelars =======================================
 
 function handelError(res) {
     return err => {
+        //log error
+        console.log(err)
         // let user know we messed up
         res.status(500).render("pages/error", { err: err });
     };
@@ -37,7 +39,8 @@ function handelError(res) {
 function home(req, res) {
     let sqlSelect = 'SELECT * FROM book'
     client.query(sqlSelect)
-        .then(books => { res.render('pages/index', { books: books.rows })
+        .then(books => {
+            res.render('pages/index', { books: books.rows })
         })
         .catch(handelError(res))
 };
@@ -51,15 +54,30 @@ function getBooks(req, res) {
     superagent.get(`https://www.googleapis.com/books/v1/volumes?q=${req.body.search}:${req.body.search_string}`)
         .then(data => {
             const books = data.body.items.map(book => new Book(book.volumeInfo));
-            res.render('pages/searches/show', { books: books })
+            res.render('pages/searches/show', { books: books, apiReturn: true })
         }).catch(handelError(res));
 };
 
 function getBook(req, res) {
     const sqlSelect = `SELECT * FROM book WHERE id=${req.params.id}`;
     client.query(sqlSelect)
-        .then(books => { res.render('pages/books/detail', { books: books.rows })
+        .then(books => {
+            res.render('pages/books/detail', { books: books.rows })
         })
+        .catch(handelError(res))
+}
+
+function saveBook(req, res) {
+    const sqlString = 'INSERT INTO book (isbn, img_url, title, author, description) VALUES($1, $2, $3, $4, $5) RETURNING id;';
+    const sqlArray = [
+        req.body.isbn,
+        req.body.img_url,
+        req.body.title,
+        req.body.author,
+        req.body.description,
+    ];
+    client.query(sqlString, sqlArray)
+        .then(result => res.redirect(`/books/${result.rows[0].id}`))
         .catch(handelError(res))
 }
 
@@ -69,7 +87,7 @@ app.use('*', (request, response) => response.send('Sorry, that route does not ex
 //constructors
 function Book(obj) {
     this.id = obj.id ? obj.id : '';
-    this.isbn = obj.isbn;
+    this.isbn = obj.industryIdentifiers[0].identifier;
     this.img_url = obj.imageLinks ? obj.imageLinks.thumbnail : "https://i.imgur.com/J5LVHEL.jpg";
     this.title = obj.title;
     this.author = obj.authors ? obj.authors[0] : 'Unknown author';
